a905967e0ce0760fb7bfe910cffa1236db615771
ashiqur-zayed-strativ <ashiqur.zayed@strativ.se>
2025-12-17 00:37:31 +0600

    fix: static files

--- jobPortal/settings.py
+++ jobPortal/settings.py
@@ -160,12 +160,15 @@ if USE_S3:
     AWS_QUERYSTRING_AUTH = False
 
 else:
-    STATIC_URL = "static/"
-    STATIC_ROOT = os.path.join(BASE_DIR, "static")
+    STATIC_URL = "/static/"
+    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
     MEDIA_URL = "/media/"
     MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, "static"),
+    ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
