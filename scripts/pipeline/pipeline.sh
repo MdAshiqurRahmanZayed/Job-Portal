352030f5989a46f40f0a80452f803ea6d2696300
MdAshiqurRahmanZayed <mdzayed2019@gmail.com>
2025-12-27 23:45:32 +0600

    hotfix: add sudo in docker command

--- scripts/pipeline/pipeline.sh
+++ scripts/pipeline/pipeline.sh
@@ -11,9 +11,9 @@ echo -e '\e[1m\e[34mPulling code from remote...\e[0m\n';
 git pull origin main;
 
 echo -e '\e[1m\e[34mBuilding image...\e[0m\n';
-docker build .;
+sudo docker build .;
 
 echo -e '\e[1m\e[34mPushing image to internal hub...\e[0m\n';
-docker-compose -f docker-compose.yml up --build;
+sudo docker-compose -f docker-compose.yml up --build;
 
 echo -e '\e[1m\e[34mDeployment Done!\e[0m\n';
